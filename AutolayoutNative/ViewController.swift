e5f27d02bed8c66353ef96ec20e0a035b2a8a855
Maxim Spiridonov <taijased@gmail.com>
2019-04-10 12:16:05 +0300

    Initial Commit

---
+++ AutolayoutNative/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutolayoutNative
+//
+//  Created by Maxim Spiridonov on 10/04/2019.
+//  Copyright © 2019 Maxim Spiridonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
